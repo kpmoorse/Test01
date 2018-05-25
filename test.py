dcdc7b89407d952cb6cdeae8d6ddc6bc23c278d7
Kellan Moorse <moorsekp@nih.gov>
2018-05-25 12:05:43 -0400

    Added hex painter

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ Created on Thu May 24 08:56:07 2018
 """
 
 #Container for display parameters
-class dispParms(object):
+class DispParms(object):
     def __init__(self):
         self.W = None
         self.H = None
@@ -14,11 +14,13 @@ class dispParms(object):
 import pygame
 import math as m
 import numpy as np
+import traceback
+import csv
 #import matplotlib.pyplot as plt
 
 pygame.init()
 
-gdisp = dispParms()
+gdisp = DispParms()
 gdisp.W = 800
 gdisp.H = 600
 gameDisplay = pygame.display.set_mode((gdisp.W,gdisp.H))
@@ -37,103 +39,182 @@ bg_bridge_lg = pygame.transform.scale(bg_bridge, (m.floor(800*1.35), m.floor(555
 bg_bridge_lgrot = pygame.transform.rotate(bg_bridge_lg, 9)
 sprite = pygame.image.load('sprite.png')
 sprite_lg = pygame.transform.scale(sprite, (49,84))
-circle = pygame.image.load('circle.png')
-circle_sm = pygame.transform.scale(circle, (20, 20))
-circle_xsm = pygame.transform.scale(circle, (6, 6))
+
+circle_r = pygame.image.load('circle-r.png')
+circle_rsm = pygame.transform.scale(circle_r, (6, 6))
+circle_rlg = pygame.transform.scale(circle_r, (10, 10))
+circle_b = pygame.image.load('circle-b.png')
+circle_bsm = pygame.transform.scale(circle_b, (6, 6))
+circle_blg = pygame.transform.scale(circle_b, (10, 10))
+
 hextile = pygame.image.load('hex.png')
-#hextile_sm = pygame.transform.scale(hextile, (100, 60))
-hextile_sm = pygame.transform.scale(hextile, (67, 80))
+hextile_sm = pygame.transform.scale(hextile, (67, 38))
 
 def gameloop():
     
-    
-    s = 40 #hex side length
-    origin = (4*3*s, 2*m.sqrt(3)*s) #location of start hex (y,x)
-    
-    gameExit = False
-    
-    pw_loc =  origin
-    
-    def pawn(y,x):
-        gameDisplay.blit(sprite_lg, (x-25,y-75))
-    def gridmarker(y,x):
-        gameDisplay.blit(circle_xsm, (x-3, y-3))
-    def marker(y,x):
-        gameDisplay.blit(circle_sm, (x-10,y-10))
-    def hexx(y,x):
-        gameDisplay.blit(hextile_sm, (x-33, y-40))
-    
-    while not gameExit:
-    
-        griddim = (m.ceil(gdisp.H/(s/2)), m.ceil(gdisp.W/(m.sqrt(3)*s)))
-        gridpts = np.zeros((griddim[0], griddim[1], 2))
-    
-        for i in range(gridpts.shape[0]):
-            for j in range(gridpts.shape[1]):
-                gridpts[i,j,:] = [(i*1.5*s), j*m.sqrt(3)*s + (i%2)*m.sqrt(3)/2*s]
+    try:
+
+        gameExit = False
         
-        mpos = pygame.mouse.get_pos()[::-1]
-        y = mpos[0]
-        x = mpos[1]
-        marker(mpos[0], mpos[1])
-        dist = np.sqrt(np.square(gridpts[:,:,0] - np.tile(y,griddim)) + np.square(gridpts[:,:,1] - np.tile(x,griddim)))
-        minidx = np.where(dist==dist.min())
-        minidx = (minidx[0][0], minidx[1][0])
-        minloc = (gridpts[minidx[0],minidx[1],0], gridpts[minidx[0],minidx[1],1])
+        #Define hex grid instance and parameters
+        hexg = HexGrid()
+        hexg.s = 40 #hex side length
+        hexg.aspect = 0.5
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                gameExit = True
+#        hexg.griddim = (m.ceil(gdisp.H/(hexg.s/2)), m.ceil(gdisp.W/(m.sqrt(3)*hexg.s)))
+#        hexg.gridpts = np.zeros((hexg.griddim[0], hexg.griddim[1], 3))
+#        for i in range(hexg.gridpts.shape[0]):
+#                for j in range(hexg.gridpts.shape[1]):
+#                    hexg.gridpts[i,j,:] = [0.5*(i*1.5*hexg.s), j*m.sqrt(3)*hexg.s + (i%2)*m.sqrt(3)/2*hexg.s, 1]
+        
+        #Open hexmap file and read tile values
+        with open('bridge_hexmap.csv', 'r') as file:
+            reader = csv.reader(file, delimiter=',')
+            rowidx = -1
+            for row in reader:
+                if rowidx == -1:
+                    hexg.griddim = np.array(row).astype(int)
+                    hexg.gridpts = np.zeros((hexg.griddim[0], hexg.griddim[1], 3))
+                else:
+                    i = m.floor(rowidx/hexg.griddim[1])
+                    j = rowidx%hexg.griddim[1]
+                    hexg.gridpts[i,j] = np.array(row).astype(float)
+                rowidx += 1
+        
+        #Place test pawn at start position
+        origin = (4*3*hexg.s, 2*m.sqrt(3)*hexg.s) #location of start hex (y,x)
+        pw_loc =  origin
+        
+        #Initialize graphic objects
+        def pawn(y,x):
+            gameDisplay.blit(sprite_lg, (x-25,y-75))
+        def gridmarker(y,x,color):
+            if color=='r': gameDisplay.blit(circle_rsm, (x-3, y-3))
+            if color=='b': gameDisplay.blit(circle_bsm, (x-3, y-3))
+        def pointer(y,x,point_mode):
+            if point_mode=='paint:red': gameDisplay.blit(circle_rlg, (x-5, y-5))
+            if point_mode=='paint:blue': gameDisplay.blit(circle_blg, (x-5, y-5))
+        def hexx(y,x):
+            gameDisplay.blit(hextile_sm, (x-33, y-20))
+        
+        #initialize pointer mode
+        point_mode = 'move'
+        
+        #Open main game loop
+        while not gameExit:
+            
+            
+            mpos = pygame.mouse.get_pos()[::-1]
+            minloc, minidx = hexg.xy_to_hex(mpos[0], mpos[1])
+            
+            
+        
+            gameDisplay.fill(white)
+            gameDisplay.blit(bg_bridge_lgrot, (-130,-150))
+            
+            for i in range(hexg.gridpts.shape[0]):
+                for j in range(hexg.gridpts.shape[1]):
+                    if hexg.gridpts[i,j,2] == 1:
+                        gridmarker(hexg.gridpts[i,j,0],hexg.gridpts[i,j,1], 'r')
+                    elif hexg.gridpts[i,j,2] == 2:
+                        gridmarker(hexg.gridpts[i,j,0],hexg.gridpts[i,j,1], 'b')
+            
+            hexx(minloc[0], minloc[1])
+            
+            # tiles = path_check(4, pw_loc, hexg.gridpts, hexg.griddim, s)
+            if point_mode == 'move':
+                tiles = hexg.path_check(4, pw_loc)
+                for tile in tiles:
+                    hexx(tile[0], tile[1])
+                pointer(mpos[0],mpos[1],point_mode)
+            else: tiles = np.empty([0,3])
                 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    pw_loc = minloc
-    
-#            if event.type == pygame.KEYDOWN:
-#                if event.key == pygame.K_LEFT:
-#                    x_change = -5
-#                elif event.key == pygame.K_RIGHT:
-#                    x_change = 5
-#            if event.type == pygame.KEYUP:
-#                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-#                    x_change = 0
-    
-            #print(event)
-    
-        gameDisplay.fill(white)
-        gameDisplay.blit(bg_bridge_lgrot, (-130,-150))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameExit = True
+                    
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        if point_mode == 'move' and np.all(tiles[:,:2]==minloc, axis=1).any():
+                            pw_loc = minloc
+                        if point_mode == 'paint:red':
+                            hexg.gridpts[minidx[0],minidx[1],2] = 1
+                        if point_mode == 'paint:blue':
+                            hexg.gridpts[minidx[0],minidx[1],2] = 2
+                if event.type == pygame.KEYDOWN:
+                    if event.key == 32:
+                        if point_mode == 'move': point_mode = 'paint:red'
+                        elif point_mode == 'paint:red': point_mode = 'paint:blue'
+                        elif point_mode == 'paint:blue': point_mode = 'move'
+                
+            pawn(pw_loc[0], pw_loc[1])
         
-        for i in range(gridpts.shape[0]):
-            for j in range(gridpts.shape[1]):
-                gridmarker(gridpts[i,j,0],gridpts[i,j,1])
+            pygame.display.update()
+            clock.tick(30)
         
-        hexx(minloc[0], minloc[1])
-        
-        dy = gridpts[:,:,0] - np.tile(pw_loc[0], griddim)
-        dx = gridpts[:,:,1] - np.tile(pw_loc[1], griddim)
-        
-        rgb_dist = np.zeros((3, griddim[0], griddim[1]))
-        rgb_dist[0] = np.round((m.sqrt(3)/3*dx - dy/3)/s) #red
-        rgb_dist[1] = np.round(-(m.sqrt(3)/3*dx + dy/3)/s) #green
-        rgb_dist[2] = np.round(2/3*dy/s) #blue
-        rgb_dist = np.max(np.abs(rgb_dist), axis=0)
-        for tile in gridpts[np.where(np.logical_and(1<=rgb_dist, rgb_dist<=3))]:
-            hexx(tile[0], tile[1])
-        pawn(pw_loc[0], pw_loc[1])
-        
-        
-#        rr = (m.sqrt(3)/3*x - y/3)/s
-#        gg = -(m.sqrt(3)/3*x + y/3)/s
-#        bb = 2/3*y/s
-        
-        #hexx(xx, yy)
-        #hexx(yy + 3/2*s*bb, xx + m.sqrt(3)*s*(bb/2+rr))
-    
-        pygame.display.update()
-        clock.tick(30)
+        with open('bridge_hexmap.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(hexg.griddim)
+            for i in range(hexg.griddim[0]):
+                for j in range(hexg.griddim[1]):
+                    writer.writerow(hexg.gridpts[i,j])
+            
+    except Exception as e:
+        traceback.print_exc()
+        return e
         
     return 0
 
-gameloop()
+class HexGrid(object):
+    
+    #Initialize instance attributes
+    def __init__(self):
+        self.s = None
+        self.aspect = None
+        self.gridpts = None
+        self.griddim = None
+    
+    #Return all tiles within <dist> tiles of <origin>, ignoring obstacles
+    def dist_check(self, dist, origin, blocked = True):
+        dy = (self.gridpts[:,:,0] - np.tile(origin[0], self.griddim))*2
+        dx = self.gridpts[:,:,1] - np.tile(origin[1], self.griddim)
+        
+        rgb_dist = np.zeros((3, self.griddim[0], self.griddim[1]))
+        rgb_dist[0] = np.round((m.sqrt(3)/3*dx - dy/3)/self.s) #red
+        rgb_dist[1] = np.round(-(m.sqrt(3)/3*dx + dy/3)/self.s) #green
+        rgb_dist[2] = np.round(2/3*dy/self.s) #blue
+        rgb_dist = np.max(np.abs(rgb_dist), axis=0)
+        
+        tiles = self.gridpts[np.where(rgb_dist == dist)]
+        if blocked:
+            tiles = tiles[np.where(tiles[:,2]==1)]
+        
+        return tiles
+    
+    #Return all tiles within <dist> tiles of <origin>, avoiding obstacles
+    def path_check(self, dist, origin, blocked = True):
+        tiles = self.dist_check(1, origin)
+        tiles_new = tiles
+        for i in range(dist-1):
+            tiles_check = tiles_new
+            tiles_new = np.empty((0,3))
+            for tile in tiles_check:
+                tiles_new = np.append(tiles_new, self.dist_check(1, tile[:2]),0)
+            tiles = np.append(tiles, tiles_new, 0)
+            tiles = tiles[np.where(np.any(tiles[:,:2]!=origin, axis=1))]
+        tiles = np.unique(tiles, axis=0)
+  
+        return tiles
+    
+    def xy_to_hex(self, y, x):
+        dist = np.sqrt(np.square(self.gridpts[:,:,0] - np.tile(y,self.griddim)) + np.square(self.gridpts[:,:,1] - np.tile(x,self.griddim)))
+        minidx = np.where(dist==dist.min())
+        minidx = (minidx[0][0], minidx[1][0])
+        minloc = (self.gridpts[minidx[0],minidx[1],0], self.gridpts[minidx[0],minidx[1],1])
+        
+        return minloc, minidx
+    
+
+e = gameloop()
 pygame.quit()
 #quit()
